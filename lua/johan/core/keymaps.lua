b064bf8f9b3bf8a2fe9f4ccd71bd48e41da9fe9f
flyingbutton <thedrafttin@gmail.com>
2025-02-07 00:34:03 -0500

    add keybindings for switching between buffers

--- lua/johan/core/keymaps.lua
+++ lua/johan/core/keymaps.lua
@@ -36,5 +36,8 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<Tab>", function () vim.cmd("bnext") end, {desc = "move to the next file in the buffer"})
+keymap.set("n", "<S-Tab>", function () vim.cmd("bprev") end, {desc = "move to the previous file in the buffer"})
 
 keymap.set("n", "<leader>aa", "ggVG", { desc = "Select all content in the file" })
+

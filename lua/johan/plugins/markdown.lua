4c3091c16b04678eb675a316792ffd7add34d1c9
flyingbutton <thedrafttin@gmail.com>
2025-04-21 15:36:09 -0400

    add render-markdown

--- lua/johan/plugins/markdown.lua
+++ lua/johan/plugins/markdown.lua
@@ -1,34 +1,43 @@
 return {
     {
-      "epwalsh/obsidian.nvim",
-      version = "*",  -- recommended, use latest release instead of latest commit
-      lazy = true,
-      ft = "markdown",
-      dependencies = {
-        -- Required.
-        "nvim-lua/plenary.nvim",
-      },
-      opts = {
-        workspaces = {
-          {
-            name = "personal",
-            path = "/Users/ningyuheng/Library/Mobile Documents/com~apple~CloudDocs/note",
-          },
-          -- {
-          --   name = "work",
-          --   path = "~/vaults/work",
-          -- },
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        dependencies = {
+            -- Required.
+            "nvim-lua/plenary.nvim",
         },
-        ui = { enable = false },
-      },
+        opts = {
+            workspaces = {
+                {
+                    name = "personal",
+                    path = "/Users/ningyuheng/Library/Mobile Documents/com~apple~CloudDocs/ObsidianVault",
+                },
+                -- {
+                --   name = "work",
+                --   path = "~/vaults/work",
+                -- },
+            },
+            ui = { enable = false },
+        },
     },
     {
-      "iamcco/markdown-preview.nvim",
-      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      build = "cd app && yarn install",
-      init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-      end,
-      ft = { "markdown" },
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
     },
 }

a45604e0fb5417a595cc5ba5357703c1777f6ba3
flyingbutton <thedrafttin@gmail.com>
2025-10-29 11:41:47 -0400

    tmux related changes

--- lua/johan/plugins/nvim-cmp.lua
+++ lua/johan/plugins/nvim-cmp.lua
@@ -29,6 +29,9 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
+                -- Move completion selection with Alt/Option+j/k
+                ["<M-j>"] = cmp.mapping.select_next_item(),
+                ["<M-k>"] = cmp.mapping.select_prev_item(),
                 ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
                 ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),

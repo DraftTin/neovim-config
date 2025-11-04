20940f78d4c40e3cfaa1e2b61c93570f67f6a2f8
flyingbutton <thedrafttin@gmail.com>
2025-11-04 12:46:07 -0500

    Added 'gruvbox' option to colorscheme

--- lua/johan/plugins/colorscheme.lua
+++ lua/johan/plugins/colorscheme.lua
@@ -1,27 +1,16 @@
 return {
-    -- {
-    --     "ellisonleao/gruvbox.nvim",
-    --     priority = 1000,
-    --     config = function()
-    --         vim.o.background = "dark" -- or "light" for light mode
-    --         vim.cmd([[colorscheme gruvbox]])
-    --     end,
-    --     opts = {
-    --         bold = true,
-    --     },
-    -- },
     {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
         config = function()
             require("catppuccin").setup({
-                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                flavour = "auto", -- latte, frappe, macchiato, mocha
                 background = { -- :h background
                     light = "latte",
-                    dark = "mocha",
+                    dark = "macchiato",
                 },
-                transparent_background = false, -- disables setting the background color.
+                transparent_background = true, -- disables setting the background color.
                 show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
                 term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
                 dim_inactve = {
@@ -65,7 +54,38 @@ return {
             })
 
             -- setup must be called before loading
-            vim.cmd.colorscheme("catppuccin")
+            -- vim.cmd.colorscheme("catppuccin")
+        end,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            -- Default options:
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = true,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = true,
+            })
+            vim.cmd.colorscheme("gruvbox")
         end,
     },
 }

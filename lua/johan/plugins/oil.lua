f591d570efed71b2cef6b6bb2dd1e250ed29419b
flyingbutton <thedrafttin@gmail.com>
2026-02-10 16:07:23 -0500

    Disabled keymaps that are conficted with tmux shortcuts

--- lua/johan/plugins/oil.lua
+++ lua/johan/plugins/oil.lua
@@ -9,7 +9,17 @@ return {
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
     config = function()
-        require("oil").setup()
+        require("oil").setup({
+            keymaps = {
+                -- Disable the default C-h and C-l to let tmux-navigator work
+                ["<C-h>"] = false,
+                ["<C-l>"] = false,
+
+                -- Optional: You can still keep the functionality by mapping them elsewhere
+                -- ["<C-x>"] = "actions.select_split",
+            },
+        })
+
         vim.keymap.set("n", "-", "<cmd>Oil<CR>")
     end,
 }

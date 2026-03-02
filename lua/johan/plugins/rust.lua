400d50251aa3840cbff2464edf5a96524e29270c
flyingbutton <thedrafttin@gmail.com>
2026-03-02 12:10:41 -0500

    Update the rust plugin version

--- lua/johan/plugins/rust.lua
+++ lua/johan/plugins/rust.lua
@@ -1,7 +1,7 @@
 return {
     {
         "mrcjkb/rustaceanvim",
-        version = "^7", -- Recommended
+        version = "^8", -- Recommended
         lazy = false, -- This plugin is already lazy
     },
     -- {
